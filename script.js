22809d8de80ff42cc9037671a585a3457ead5eef
Pavan <pavanganigera27753@gmail.com>
2025-08-22 12:09:56 +0530

    js changes done

--- script.js
+++ script.js
@@ -25,11 +25,11 @@ const typed = new Typed('.multiple-text', {
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
-function opentab(tabname) {
-    for (tablink of tablinks) {
+function opentab(tabname, event) {
+    for (let tablink of tablinks) {
         tablink.classList.remove("active-link");
     }
-    for (tabcontent of tabcontents) {
+    for (let tabcontent of tabcontents) {
         tabcontent.classList.remove("active-tab");
     }
     event.currentTarget.classList.add("active-link");
